b8043f6d9de4cfec5d3daefd76a05c9196739727
Erlon Silva Santos <erlon.santos@simply.com.br>
2024-01-18 16:17:39 -0300

    first commit

---
+++ RealTimeBroker/RealTimeBroker.Web/Program.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+
+using RealTimeBroker.Web.HostedServices;
+using RealTimeBroker.Web.Hubs;
+
+namespace RealTimeBroker.Web
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+
+      
+
+ 
+
+      builder.Services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy", builder => builder
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+      });
+
+      builder.Services.AddControllers();
+
+      builder.Services.AddSignalR();
+      builder.Services.AddHostedService<UpdateStockPriceHostedService>();
+
+
+      var app = builder.Build();
+
+      app.UseHttpsRedirection();
+      app.UseCors("CorsPolicy");
+
+      app.MapHub<BrokerHub>("/brokerhub");
+
+      app.Run();
+
+    }
+
+
+  }
+}
---
+++ front-dashboard-stocks/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  dictStocks = {
+    "ITSA4": {
+      startValue: 20.0,
+      currentValue: 20.0,
+      change: 0.0
+    },
+    "TAEE11": {
+      startValue: 20.0,
+      currentValue: 20.0,
+      change: 0.0
+    },
+    "PETR4": {
+      startValue: 20.0,
+      currentValue: 20.0,
+      change: 0.0
+    }
+  };
+
+  private _hubConnection: HubConnection;
+  
+  constructor() {  
+    this.createConnection();  
+    this.registerOnServerEvents();  
+    this.startConnection();  
+  }  
+  
+  connectToStock(symbol: string) {  
+    this._hubConnection.invoke('ConnectToStock', symbol);  
+  }  
+  
+  private createConnection() {  
+    this._hubConnection = new HubConnectionBuilder()  
+      .withUrl('https://localhost:44390/brokerhub')  
+      .build();  
+  }  
+  
+  private startConnection(): void {  
+    this._hubConnection  
+      .start()  
+      .then(() => {   
+        for (let key in this.dictStocks) {
+          this.connectToStock(key);
+        }
+      })  
+      .catch(() => {  
+        setTimeout(function () { this.startConnection(); }, 5000);  
+      });  
+  }  
+  
+  private registerOnServerEvents(): void {  
+    this._hubConnection.on('UpdatePrice', (data: any) => {
+      let item = this.dictStocks[data.symbol];
+      item.currentValue = data.price;
+      item.change = (item.currentValue - item.startValue)/item.startValue;
+    });  
+  }  
+}
